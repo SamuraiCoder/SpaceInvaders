613a544f36f74ae4130087dde0ae01ca4926eb83
Sam <sam.alonso@gmail.com>
2021-03-27 14:25:13 +0100

    Create skeleton class for Enemy Ship

---
+++ Assets/Scripts/Controllers/EnemyShipController.cs
@@ -0,0 +1,48 @@
+﻿using Events;
+using pEventBus;
+using UnityEngine;
+
+namespace Controllers
+{
+    public class EnemyShipController : BaseShipEntity, IEventReceiver<EnemyShipMoveEvent>
+    {
+        private float enemySpeed;
+        private Vector2 enemyDirection;
+
+        protected override void Start()
+        {
+            base.Start();
+            EventBus.Register(this);
+        }
+
+        private void OnDestroy()
+        {
+            EventBus.UnRegister(this);
+        }
+
+        protected override void Update()
+        {
+            base.Update();
+
+            OnEnemyAIInputReceived();
+        }
+
+        private void OnEnemyAIInputReceived()
+        {
+            direction = enemyDirection;
+            speed = enemySpeed;
+        }
+
+        public void OnEvent(EnemyShipMoveEvent e)
+        {
+            if (e.EnemyShipName != gameObject.name)
+            {
+                return;
+            }
+            
+            enemyDirection = e.Direction;
+            enemySpeed = e.Speed;
+        }
+    }
+}
+
---
+++ Assets/Scripts/Events/EnemyShipMoveEvent.cs
@@ -0,0 +1,13 @@
+﻿using pEventBus;
+using UnityEngine;
+
+namespace Events
+{
+    public struct EnemyShipMoveEvent : IEvent
+    {
+        public string EnemyShipName { get; set; }
+        public Vector2 Direction { get; set; }
+        public float Speed { get; set; }
+    }
+}
+
